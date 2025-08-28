36062c1e4502eabb39c237597f1b8c1f668637fb
dewurugenuvin <dewurugenuvin@yahoo.com>
2025-08-29 00:48:33 +0530

    Add models

---
+++ src/main/java/com/example/library/model/Book.java
@@ -0,0 +1,24 @@
+package com.example.library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String isbn;
+    private String title;
+    private String author;
+
+    @ManyToOne
+    private Borrower borrower; // nullable if not borrowed
+}
